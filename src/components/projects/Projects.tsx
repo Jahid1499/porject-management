f0ae83a78e73310fbe6e58574364e1e3773778ab
jahid <jahid15-1499@diu.edu.bd>
2024-05-28 21:19:31 +0600

    Feat: search functionality done

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -151,7 +151,7 @@ const Projects = () => {
   };
 
   return (
-    <Template>
+    <Template searchOption={true}>
       <div className="px-10 mt-6">
         <h1 className="text-2xl font-bold">Project Board</h1>
       </div>
